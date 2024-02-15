5d71101285c6b87f29b4c1c55a6211d577b9685c
Yuri2224 <158284112+Yuri2224@users.noreply.github.com>
2024-02-15 11:41:02 -0300

    Update script.js

---
+++ Activity02/script.js
@@ -0,0 +1,26 @@
+var nomeGlobal;
+var mensagemGlobal;
+
+function conferirMensagemWhatsApp(){
+var nome = document.getElementById("nome").value;
+var mensagem = document.getElementById("mensagem").value;
+console.log(nome, mensagem)
+nomeGlobal = nome;
+mensagemGlobal = mensagem;
+
+document.getElementById("confNome").textContent = nome;
+document.getElementById("confMsg").textContent = mensagem;
+}
+
+function enviar(){
+    var numeroTelefone = "5541991313672"
+
+    var linkWhatsApp = "https://wa.me/" +
+     numeroTelefone + 
+     "?text=Nome: " + 
+     nomeGlobal +
+     " - " +
+     mensagemGlobal;
+
+    window.open(linkWhatsApp, "_blank");
+}
